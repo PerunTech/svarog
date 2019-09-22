3a294f26fbc5576b081e626fe03a72ae35f812c0
r.pejov <r.pejov@prtech.mk>
2019-09-22 15:35:42 +0200

    Update of the licence header and added pre-install crosschecks

--- src/com/prtech/svarog/DbInit.java
+++ src/com/prtech/svarog/DbInit.java
@@ -1,17 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2017 Perun Technologii DOOEL Skopje.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Apache License
- * Version 2.0 or the Svarog License Agreement (the "License");
- * You may not use this file except in compliance with the License. 
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See LICENSE file in the project root for the specific language governing 
- * permissions and limitations under the License.
- *
+ *   Copyright (c) 2013, 2019 Perun Technologii DOOEL Skopje.
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Apache License
+ *   Version 2.0 or the Svarog License Agreement (the "License");
+ *   You may not use this file except in compliance with the License. 
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See LICENSE file in the project root for the specific language governing 
+ *   permissions and limitations under the License.
+ *  
  *******************************************************************************/
+ 
 package com.prtech.svarog;
 
 import com.prtech.svarog.SvCore.SvAccess;
@@ -4898,21 +4899,23 @@ public class DbInit {
 		dbtList.add(addSortOrder(dbtt));
 
 		// Add SDI structure
-		dbtt = getSDIMasterRepoObject();
-		dbtList.add(addSortOrder(dbtt));
-		dbtt = getMasterSDIUnits();
-		dbtList.add(addSortOrder(dbtt));
-		dbtt = getMasterSDIBounds();
-		dbtList.add(addSortOrder(dbtt));
-		dbtt = getMasterSDICover();
-		dbtList.add(addSortOrder(dbtt));
-		dbtt = getMasterSDIUse();
-		dbtList.add(addSortOrder(dbtt));
-		dbtt = getMasterSDIDescriptor();
-		dbtList.add(addSortOrder(dbtt));
-		dbtt = getMasterSDIService();
-		dbtList.add(addSortOrder(dbtt));
-
+		if (SvConf.isSdiEnabled()) {
+			dbtt = getSDIMasterRepoObject();
+			dbtList.add(addSortOrder(dbtt));
+			dbtt = getMasterSDIUnits();
+			dbtList.add(addSortOrder(dbtt));
+			dbtt = getMasterSDIBounds();
+			dbtList.add(addSortOrder(dbtt));
+			dbtt = getMasterSDICover();
+			dbtList.add(addSortOrder(dbtt));
+			dbtt = getMasterSDIUse();
+			dbtList.add(addSortOrder(dbtt));
+			dbtt = getMasterSDIDescriptor();
+			dbtList.add(addSortOrder(dbtt));
+			dbtt = getMasterSDIService();
+			dbtList.add(addSortOrder(dbtt));
+		} else
+			log4j.info("Spatial Data is disabled. User paramater sys.gis.enable_spatial to set it");
 		dbtt = getMasterNotes();
 		dbtList.add(addSortOrder(dbtt));
 
@@ -5370,8 +5373,7 @@ public class DbInit {
 			// master_locales.json", locales.toJson().toString());
 			for (DbDataObject entry : locales.getItems()) {
 				try {
-					String labelFile = "/" + svCONST.masterCodesPath + entry.getVal("locale_id")
-							+ "Labels.properties";
+					String labelFile = "/" + svCONST.masterCodesPath + entry.getVal("locale_id") + "Labels.properties";
 
 					InputStream iStr = DbInit.class.getResourceAsStream(labelFile);
 					if (iStr == null)
--- src/com/prtech/svarog/SvGeometry.java
+++ src/com/prtech/svarog/SvGeometry.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2017 Perun Technologii DOOEL Skopje.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Apache License
- * Version 2.0 or the Svarog License Agreement (the "License");
- * You may not use this file except in compliance with the License. 
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See LICENSE file in the project root for the specific language governing 
- * permissions and limitations under the License.
- *
+ *   Copyright (c) 2013, 2019 Perun Technologii DOOEL Skopje.
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Apache License
+ *   Version 2.0 or the Svarog License Agreement (the "License");
+ *   You may not use this file except in compliance with the License. 
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See LICENSE file in the project root for the specific language governing 
+ *   permissions and limitations under the License.
+ *  
  *******************************************************************************/
 package com.prtech.svarog;
 
@@ -165,7 +165,7 @@ public class SvGeometry extends SvCore {
 	}
 
 	// private static Boolean initGrid = initGrid();
-	private static boolean initSDI() {
+	static boolean initSDI() {
 		try {
 			admUnitClass = Long.parseLong(SvConf.getParam("sys.gis.legal_sdi_unit_type"));
 
@@ -645,6 +645,10 @@ public class SvGeometry extends SvCore {
 	public void setAllowNullGeometry(boolean allowNullGeometry) {
 		if (this.hasPermission(svCONST.NULL_GEOMETRY_ACL))
 			this.allowNullGeometry = allowNullGeometry;
+	}
+
+	public static boolean isSDIInitalized() {
+		return isSDIInitalized;
 	}
 
 }
