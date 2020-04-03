e5ae8954f947359bd8ffa685d3ba7507e39a75fc
r.pejov <r.pejov@prtech.mk>
2020-04-03 10:31:33 +0200

    SvCore.isInternal is inherited from parent

--- src/com/prtech/svarog/SvCore.java
+++ src/com/prtech/svarog/SvCore.java
@@ -422,7 +422,8 @@ public abstract class SvCore implements ISvCore {
 				throw (new SvException("system.error.cant_use_svsec_as_core", instanceUser));
 
 		this.instanceUser = (srcCore != null ? srcCore.instanceUser : instanceUser);
-
+		this.isInternal = (srcCore != null ? srcCore.isInternal : this.isInternal);
+		
 		if (this.instanceUser == null)
 			throw (new SvException("system.error.instance_user_null", svCONST.systemUser));
 
