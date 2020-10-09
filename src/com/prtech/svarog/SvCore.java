5bc4ded77726f8e3f3ffe636088aec28e26b975b
r.pejov <r.pejov@prtech.mk>
2020-10-09 10:55:17 +0200

    Ensure proper handling of JSON formatting errors in GUI_METADATA

--- src/com/prtech/svarog/SvCore.java
+++ src/com/prtech/svarog/SvCore.java
@@ -1109,22 +1109,21 @@ public abstract class SvCore implements ISvCore, java.lang.AutoCloseable {
 	 */
 	private static void prepareFields(DbDataArray fields) throws SvException {
 		for (DbDataObject dbo : fields.getItems()) {
-			Gson gs = new Gson();
-			if (dbo.getVal(Sv.GUI_METADATA) != null) {
-				try {
+			try {
+				Gson gs = new Gson();
+				if (dbo.getVal(Sv.GUI_METADATA) != null) {
 					JsonObject jo = gs.fromJson((String) dbo.getVal(Sv.GUI_METADATA), JsonObject.class);
 					dbo.setVal(Sv.GUI_METADATA, jo);
 					dbo.setIsDirty(false);
-				} catch (Exception e) {
-					log4j.warn("Field :" + dbo.getVal("field_name") + " has non-JSON gui metadata");
 				}
+				if (dbo.getVal(Sv.EXTENDED_PARAMS) != null)
+					prepareExtOpts(dbo);
+				// finally link the ext params and gui metadata to the parent
+				// meta
+				linkToParentDbt(dbo);
+			} catch (Exception e) {
+				log4j.warn("Field :" + dbo.getVal("field_name") + " has non-JSON gui metadata", e);
 			}
-
-			if (dbo.getVal(Sv.EXTENDED_PARAMS) != null)
-				prepareExtOpts(dbo);
-			// finally link the ext params and gui metadata to the parent meta
-			linkToParentDbt(dbo);
-
 		}
 	}
 
