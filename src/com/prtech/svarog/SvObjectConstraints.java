1f370d8d689f0c4d391748366fb1cfb3336b8696
r.pejov <r.pejov@prtech.mk>
2021-01-19 09:30:35 +0100

    Fix constraints multithreading issue

--- src/com/prtech/svarog/SvObjectConstraints.java
+++ src/com/prtech/svarog/SvObjectConstraints.java
@@ -25,41 +25,35 @@ public class SvObjectConstraints {
 	DbDataObject dbt;
 	DbDataArray dbtFields;
 
-
-	public SvObjectConstraints(DbDataObject dbt) throws SvException
-	{
-		this.dbt=dbt;
-		dbtFields = SvCore.getFields(dbt.getObject_id());
-		if(dbtFields!=null)
-		{
-			for(DbDataObject dbo: dbtFields.getItems())
-			{
-				if((Boolean)dbo.getVal("is_unique"))
-				{
-					String unqName = (String)dbo.getVal("unq_constraint_name");
+	SvObjectConstraints(DbDataObject dbt) throws SvException {
+		this.dbt = dbt;
+		dbtFields = SvCore.getFields(dbt.getObjectId());
+		if (dbtFields != null) {
+			for (DbDataObject dbo : dbtFields.getItems()) {
+				if ((Boolean) dbo.getVal("is_unique")) {
+					String unqName = (String) dbo.getVal("unq_constraint_name");
 					unqName = (unqName == null ? "DEFAULT_UNQ" : unqName);
-					SvConstraint currentCons=constraints.get(unqName);
-					if(currentCons==null)
-					{
-						currentCons=new SvConstraint(this,unqName);
+					SvConstraint currentCons = constraints.get(unqName);
+					if (currentCons == null) {
+						currentCons = new SvConstraint(this, unqName);
 						constraints.put(unqName, currentCons);
 					}
-					if(!currentCons.addField(dbo))
-						throw(new SvException("system.error.bad_constraint_cfg", svCONST.systemUser,dbt,dbo));
-						
+					if (!currentCons.addField(dbo))
+						throw (new SvException("system.error.bad_constraint_cfg", svCONST.systemUser, dbt, dbo));
+
 				}
 			}
 		}
+		for (SvConstraint svc : constraints.values())
+			svc.initSQL();
 	}
-	public StringBuilder getSQLQueryString(DbDataArray dba)
-	{
-		StringBuilder retStrB=null;
-		for(SvConstraint svc:constraints.values())
-		{
-			if(retStrB==null)
-				retStrB=svc.getSQLQueryString(dba);
-			else 
-			{
+
+	public StringBuilder getSQLQueryString(DbDataArray dba) {
+		StringBuilder retStrB = null;
+		for (SvConstraint svc : constraints.values()) {
+			if (retStrB == null)
+				retStrB = svc.getSQLQueryString(dba);
+			else {
 				retStrB.append(" UNION ");
 				retStrB.append(svc.getSQLQueryString(dba));
 			}
@@ -67,45 +61,38 @@ public class SvObjectConstraints {
 		return retStrB;
 	}
 
-	public ArrayList<Object> getSQLParamVals(DbDataArray dba)
-	{
-		ArrayList<Object> retVals=null;
-		for(SvConstraint svc:constraints.values())
-		{
-			if(retVals==null)
-				retVals=svc.getSQLParamVals(dba);
-			else 
-			{
-				
+	public ArrayList<Object> getSQLParamVals(DbDataArray dba) {
+		ArrayList<Object> retVals = null;
+		for (SvConstraint svc : constraints.values()) {
+			if (retVals == null)
+				retVals = svc.getSQLParamVals(dba);
+			else {
+
 				retVals.addAll(svc.getSQLParamVals(dba));
 			}
 		}
 		return retVals;
 	}
+
 	public HashMap<String, SvConstraint> getConstraints() {
 		return constraints;
 	}
-
 
 	public void setConstraints(HashMap<String, SvConstraint> constraints) {
 		this.constraints = constraints;
 	}
 
-
 	public DbDataObject getDbt() {
 		return dbt;
 	}
-
 
 	public void setDbt(DbDataObject dbt) {
 		this.dbt = dbt;
 	}
 
-
 	public DbDataArray getDbtFields() {
 		return dbtFields;
 	}
-
 
 	public void setDbtFields(DbDataArray dbtFields) {
 		this.dbtFields = dbtFields;
